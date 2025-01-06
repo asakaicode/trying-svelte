38bcdbdfe190b2d853de66543ff04ecbdcb0e0da
asakaicode <tubakaito5100@gmail.com>
2025-01-07 08:19:08 +0900

    feat: add simple state actions

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	let count = $state(0);
+
+	const handleClick = () => {
+		count += 1;
+	};
+</script>
+
+<button onclick={handleClick}>
+	Clicked {count}
+	{count === 1 ? 'time' : 'times'}
+</button>
